ff7943c2f1d0e8d0a0e85157e3023da90d887cc4
becca <r.alexis.tolentino@gmail.com>
2017-09-19 15:49:01 -0500

    For loop using long squared Same as do while only inline

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,12 +4,14 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i*=i;
-        }while (i <= 1000000);
-        // in this case, we should use long, since the number is much bigger than you would use with just an integer. Using an integer would break or continue the loop, IT SHOULD NOT BE USED. just use long.
+       for (long i = 2; i<= 1000000;i*=i) {
+
+           System.out.println(i);
+       }
+
+       //for loop squared same as do while loop only inline.
+
+
 
     }
 }
