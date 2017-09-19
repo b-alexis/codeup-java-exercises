a6ccf44b0dcbfc2ed297466760f2f00924c95f16
becca <r.alexis.tolentino@gmail.com>
2017-09-19 16:09:49 -0500

    FizzBuzz in for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,12 +4,24 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-       for (long i = 2; i<= 1000000;i*=i) {
+       for (int counter = 1; counter <=100; counter++) {
 
-           System.out.println(i);
+           if(counter % 3 ==0 && counter % 5 ==0) {
+               System.out.println("FizzBuzz");
+               continue;
+           }
+          else if(counter % 3 == 0){
+               System.out.println("Fizz");
+               continue;
+           }
+            else if(counter % 5 == 0){
+               System.out.println("Buzz");
+
+           }
+           System.out.println("counter =" + counter);
        }
 
-       //for loop squared same as do while loop only inline.
+
 
 
 
