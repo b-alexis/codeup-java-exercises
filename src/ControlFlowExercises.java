861b088314145ec2ab54580aa1de0e290d627fb7
becca <r.alexis.tolentino@gmail.com>
2017-09-19 14:46:48 -0500

    Do while loop Java increment by 2 up to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i); // increment 5 by one until 15
-            i++;
-            System.out.println();
-        }
+        int i =0;
+        do {
+            System.out.println(i*2); //i
+            i++; //i+=2
+        }while (i <= 50); //100
+
 
     }
 }
