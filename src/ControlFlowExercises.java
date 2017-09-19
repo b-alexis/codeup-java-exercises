ecfdbb0bbe81364e46c82bf2d14c3320da101b64
becca <r.alexis.tolentino@gmail.com>
2017-09-19 14:32:04 -0500

    while loop in Java

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i); // increment 5 by one until 15
+            i++;
+            System.out.println();
+        }
+
+    }
+}
