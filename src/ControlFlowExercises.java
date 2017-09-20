eb662943b8bfc0e3a3b53b4c4c593f640c8094ed
becca <r.alexis.tolentino@gmail.com>
2017-09-20 10:43:02 -0500

    grade system in java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        long c = 2;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int number = input.nextInt();
-        System.out.println("number | squared | cubed\n" + "------ | ------ | ------");
 
-        for (int j = 1; j <= number; j++) {
-            System.out.printf("%-7s|", j);
-            System.out.printf("%-8s|", (j * j));
-            System.out.printf("%-5s", (j * j * j));
-            System.out.println();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give me the grade");
+        int grade = input.nextInt();
+        input.nextLine();
+        if (grade <= 100 && grade >= 88) {
+            System.out.println("A");
+        } else if (grade <= 87 && grade >= 80) {
+            System.out.println("B");
+        } else if (grade <= 79 && grade >= 67) {
+            System.out.println("C");
+        } else if (grade <= 66 && grade >= 60) {
+            System.out.println("D");
+        } else if (grade <= 59) {
+            System.out.print("F");
         }
+
+
     }
 }
