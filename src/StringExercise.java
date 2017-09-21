426aa694b62c2c48eca357038c264b1476853540
becca <r.alexis.tolentino@gmail.com>
2017-09-21 09:18:16 -0500

    Strings inn Java

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = ("We don't need no education\n" +
+                "We don't need no thought control");
+        System.out.println(message);
+
+        message = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+
+        message = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+
+
+    }
+}
