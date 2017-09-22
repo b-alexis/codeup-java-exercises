76f3fc0e04c70eb8104eb57dc70f06899695b7a0
becca <r.alexis.tolentino@gmail.com>
2017-09-22 09:13:54 -0500

    Public static int methods

---
+++ src/MethodsExercise.java
@@ -0,0 +1,25 @@
+public class MethodsExercise {
+    public static void main(String[] args) {
+        System.out.println(addition(7, 3));
+        System.out.println(subtraction(7, 3));
+        System.out.println(multiplication(7, 3));
+        System.out.println(division (7,3));
+    }
+
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtraction(int x, int y) {
+        return x - y;
+
+    }
+
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public static int division(int x, int y) {
+        return x / y;
+    }
+}
