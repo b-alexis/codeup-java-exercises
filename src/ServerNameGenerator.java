ccc33ce67df2745562af2f13e041a540af5ad62f
becca <r.alexis.tolentino@gmail.com>
2017-09-26 16:02:04 -0500

    Create constructor methods

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,14 +1,21 @@
+import java.util.Random;
 public class ServerNameGenerator {
 
-    String[] Nouns =
+    private static String[] Nouns =
             {"fast", "moving", "forward", "learning", "quick", "awesome", "amazing", "boring", "lame", "cute"};
 
 
-    String[] Adjatives =
+   private static String[] Adjectives =
             {"flower", "lizard", "kitten", "frogFish", "turtle", "pants", "sweater", "Mountain", "lion", "dolphin"};
 
     public static void main(String[] args) {
+    String getName = getRand(Adjectives) + "-" + getRand(Nouns);
+    System.out.println("Here is your server name!:\n" + getName);
 
     }
+    private static Random rand = new Random();
+public static String getRand(String[] wordArray){
+    return wordArray
+}
 
 }
