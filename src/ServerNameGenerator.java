9e30fa4e4079e1b5a10ea42d47650092b76aeb79
becca <r.alexis.tolentino@gmail.com>
2017-09-26 14:58:24 -0500

    Create 2 different string arrays and created a main method

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,14 @@
+public class ServerNameGenerator {
+
+    String[] Nouns =
+            {"fast", "moving", "forward", "learning", "quick", "awesome", "amazing", "boring", "lame", "cute"};
+
+
+    String[] Adjatives =
+            {"flower", "lizard", "kitten", "frogFish", "turtle", "pants", "sweater", "Mountain", "lion", "dolphin"};
+
+    public static void main(String[] args) {
+
+    }
+
+}
